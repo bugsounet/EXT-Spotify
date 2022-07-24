698732533aa95cab6ff6c26c958b30678ae3e57c
bugsounet <bugsounet@bugsounet.fr>
2022-07-24 18:00:58 +0200

    add a part of greek translation (need review)

--- EXT-Spotify.js
+++ EXT-Spotify.js
@@ -128,7 +128,8 @@ Module.register("EXT-Spotify", {
       es: "translations/es.json",
       nl: "translations/nl.json",
       pt: "translations/pt.json",
-      ko: "translations/ko.json"
+      ko: "translations/ko.json",
+      el: "translations/el.json"
     }
   },
 
