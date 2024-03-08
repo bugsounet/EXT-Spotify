85194dd3933d2a1e64957f1d0425220485e19686
bugsounet <github@bugsounet.fr>
2024-03-08 09:51:51 +0100

    eslint

--- installer/auth_Spotify.js
+++ installer/auth_Spotify.js
@@ -1,38 +1,54 @@
 /** Spotify setup **/
 /** @bugsounet **/
 
-const fs = require("fs")
-const path = require("path")
-const Spotify = require("../components/spotifyLib.js")
+const fs = require("fs");
+const path = require("path");
+const Spotify = require("../components/spotifyLib.js");
 
-let file = path.resolve(__dirname, "../../../config/config.js")
-let found = false
-let config = {}
+let file = path.resolve(__dirname, "../../../config/config.js");
+let found = false;
+let config = {};
 
 if (fs.existsSync(file)) {
-  var MMConfig = require(file)
-  var MMModules = MMConfig.modules
+  var MMConfig = require(file);
+  var MMModules = MMConfig.modules;
+} else {
+  console.log("config.js not found !?");
+  process.exit();
 }
-else return console.log("config.js not found !?")
 
 for (let [nb, module] of Object.entries(MMModules)) {
-  if (module.module == "EXT-Spotify") {
-    found = true
-    if (!module.config) return console.log("Please configure this module in config.js file before!")
-    if (!module.config.CLIENT_SECRET) return console.log("CLIENT_SECRET is not defined in spotify module config !")
-    if (!module.config.CLIENT_ID) return console.log("CLIENT_ID is not defined in spotify module config !")
+  if (module.module === "EXT-Spotify") {
+    found = true;
+    if (!module.config) {
+      console.log("Please configure this module in config.js file before!");
+      process.exit();
+    }
+    if (!module.config.CLIENT_SECRET) {
+      console.log("CLIENT_SECRET is not defined in spotify module config !");
+      process.exit();
+    }
+    if (!module.config.CLIENT_ID) {
+      console.log("CLIENT_ID is not defined in spotify module config !");
+      process.exit();
+    }
+
     /** All is Good ! **/
-    config.TOKEN = "./tokenSpotify.json"
-    config.CLIENT_SECRET = module.config.CLIENT_SECRET
-    config.CLIENT_ID = module.config.CLIENT_ID
-    config.PATH = "../"
+    config.TOKEN = "./tokenSpotify.json";
+    config.CLIENT_SECRET = module.config.CLIENT_SECRET;
+    config.CLIENT_ID = module.config.CLIENT_ID;
+    config.PATH = "../";
   }
 }
-if (!found) return console.log("EXT-Spotify not configured in config.js")
 
-let Auth = new Spotify(config, null, true, true)
+if (!found) {
+  console.log("EXT-Spotify not configured in config.js");
+  process.exit();
+}
+
+let Auth = new Spotify(config, null, true, true);
 Auth.authFlow(() => {
-  console.log("[SPOTIFY_AUTH] Authorization is finished. Check ", config.TOKEN)
+  console.log("[SPOTIFY_AUTH] Authorization is finished. Check ", config.TOKEN);
 }, (e) => {
-  console.error(e)
-})
+  console.error(e);
+});
--- node_helper.js
+++ node_helper.js
@@ -1,221 +1,223 @@
-"use strict"
+"use strict";
 
-var NodeHelper = require("node_helper")
-var logSpotify = (...args) => { /* do nothing */ }
+const path = require("path");
+const fs = require("fs");
+var NodeHelper = require("node_helper");
 
-const spotify = require("./components/spotifyLib.js")
-const path = require("path")
-const fs = require("fs")
+var logSpotify = (...args) => { /* do nothing */ };
+const spotify = require("./components/spotifyLib.js");
 
 module.exports = NodeHelper.create({
-  start: function () {
-    this.retry = null
-    this.timeout= null
-    this.retryPlayerCount = 0
+  start () {
+    this.retry = null;
+    this.timeout= null;
+    this.retryPlayerCount = 0;
   },
 
-  socketNotificationReceived: function (noti, payload) {
+  socketNotificationReceived (noti, payload) {
     switch (noti) {
       case "INIT":
-        console.log("[SPOTIFY] EXT-Spotify Version:", require('./package.json').version, "rev:", require('./package.json').rev)
-        this.initialize(payload)
-      break
+        console.log("[SPOTIFY] EXT-Spotify Version:", require("./package.json").version, "rev:", require("./package.json").rev);
+        this.initialize(payload);
+        break;
       /** Spotify module **/
       case "SPOTIFY_RETRY_PLAY":
-        clearTimeout(this.timeout)
-        this.timeout= null
-        clearTimeout(this.retry)
-        this.retry = null
+        clearTimeout(this.timeout);
+        this.timeout= null;
+        clearTimeout(this.retry);
+        this.retry = null;
         this.retry = setTimeout(() => {
           this.spotify.play(payload, (code, error, result) => {
-            if ((code == 404) && (result.error.reason == "NO_ACTIVE_DEVICE")) {
-              logSpotify("RETRY playing...")
-              this.socketNotificationReceived("SPOTIFY_PLAY", payload)
+            if ((code === 404) && (result.error.reason === "NO_ACTIVE_DEVICE")) {
+              logSpotify("RETRY playing...");
+              this.socketNotificationReceived("SPOTIFY_PLAY", payload);
             }
             if ((code !== 204) && (code !== 202)) {
               if (this.config.player.usePlayer)
-                this.sendSocketNotification("WARNING", { message: "PlayerNoResponse", values: this.config.player.deviceName })
-              return console.log("[SPOTIFY:PLAY] RETRY Error", code, error, result)
+                this.sendSocketNotification("WARNING", { message: "PlayerNoResponse", values: this.config.player.deviceName });
+              return console.log("[SPOTIFY:PLAY] RETRY Error", code, error, result);
             }
             else {
-              logSpotify("RETRY: DONE_PLAY")
-              this.retryPlayerCount = 0
-              if (this.config.player.usePlayer) this.sendSocketNotification("INFORMATION", { message: "PlayerConnected", values: this.config.player.deviceName })
+              logSpotify("RETRY: DONE_PLAY");
+              this.retryPlayerCount = 0;
+              if (this.config.player.usePlayer) this.sendSocketNotification("INFORMATION", { message: "PlayerConnected", values: this.config.player.deviceName });
             }
-          })
-        }, 3000)
-        break
+          });
+        }, 3000);
+        break;
       case "SPOTIFY_PLAY":
         this.spotify.play(payload, (code, error, result) => {
-          clearTimeout(this.timeout)
-          this.timeout= null
-          if ((code == 404) && (result.error.reason == "NO_ACTIVE_DEVICE")) {
-            this.retryPlayerCount++
-            if (this.retryPlayerCount >= 4) return this.retryPlayerCount = 0
+          clearTimeout(this.timeout);
+          this.timeout= null;
+          if ((code === 404) && (result.error.reason === "NO_ACTIVE_DEVICE")) {
+            this.retryPlayerCount++;
+            if (this.retryPlayerCount >= 4) return this.retryPlayerCount = 0;
             if (this.config.player.usePlayer) {
-              console.log("[SPOTIFY] No response from player !")
-              this.sendSocketNotification("INFORMATION", { message: "PlayerConnecting" })
-              this.sendSocketNotification("PLAYER_RECONNECT")
+              console.log("[SPOTIFY] No response from player !");
+              this.sendSocketNotification("INFORMATION", { message: "PlayerConnecting" });
+              this.sendSocketNotification("PLAYER_RECONNECT");
               this.timeout= setTimeout(() => {
-                this.socketNotificationReceived("SPOTIFY_TRANSFER", this.config.player.deviceName)
-                this.socketNotificationReceived("SPOTIFY_RETRY_PLAY", payload)
-              }, 3000)
+                this.socketNotificationReceived("SPOTIFY_TRANSFER", this.config.player.deviceName);
+                this.socketNotificationReceived("SPOTIFY_RETRY_PLAY", payload);
+              }, 3000);
             }
           }
           if ((code !== 204) && (code !== 202)) {
-            return console.log("[SPOTIFY:PLAY] Error", code, result)
+            return console.log("[SPOTIFY:PLAY] Error", code, result);
           }
           else {
-            logSpotify("DONE_PLAY")
-            this.retryPlayerCount = 0
+            logSpotify("DONE_PLAY");
+            this.retryPlayerCount = 0;
           }
-        })
-        break
+        });
+        break;
       case "SPOTIFY_VOLUME":
         this.spotify.volume(payload, (code, error, result) => {
-          if (code !== 204) console.log("[SPOTIFY:VOLUME] Error", code, result)
+          if (code !== 204) console.log("[SPOTIFY:VOLUME] Error", code, result);
           else {
-            this.sendSocketNotification("DONE_SPOTIFY_VOLUME", payload)
-            logSpotify("DONE_VOLUME:", payload)
+            this.sendSocketNotification("DONE_SPOTIFY_VOLUME", payload);
+            logSpotify("DONE_VOLUME:", payload);
           }
-        })
-        break
+        });
+        break;
       case "SPOTIFY_PAUSE":
         this.spotify.pause((code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:PAUSE] Error", code, result)
-          else logSpotify("DONE_PAUSE")
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:PAUSE] Error", code, result);
+          else logSpotify("DONE_PAUSE");
+        });
+        break;
       case "SPOTIFY_TRANSFER":
         this.spotify.transferByName(payload, (code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:TRANSFER] Error", code, result)
-          else logSpotify("DONE_TRANSFER", payload)
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:TRANSFER] Error", code, result);
+          else logSpotify("DONE_TRANSFER", payload);
+        });
+        break;
       case "SPOTIFY_TRANSFER_BYID":
         this.spotify.transferById(payload, (code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:TRANSFERID] Error", code, result)
-          else logSpotify("DONE_TRANSFERID", payload)
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:TRANSFERID] Error", code, result);
+          else logSpotify("DONE_TRANSFERID", payload);
+        });
+        break;
       case "SPOTIFY_STOP":
-        if (this.config.player.usePlayer) this.sendSocketNotification("PLAYER_RECONNECT")
-        break
+        if (this.config.player.usePlayer) this.sendSocketNotification("PLAYER_RECONNECT");
+        break;
       case "SPOTIFY_NEXT":
         this.spotify.next((code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:NEXT] Error", code, result)
-          else logSpotify("DONE_NEXT")
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:NEXT] Error", code, result);
+          else logSpotify("DONE_NEXT");
+        });
+        break;
       case "SPOTIFY_PREVIOUS":
         this.spotify.previous((code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:PREVIOUS] Error", code, result)
-          else logSpotify("DONE_PREVIOUS")
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:PREVIOUS] Error", code, result);
+          else logSpotify("DONE_PREVIOUS");
+        });
+        break;
       case "SPOTIFY_SHUFFLE":
         this.spotify.shuffle(payload,(code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:SHUFFLE] Error", code, result)
-          else logSpotify("DONE_SHUFFLE")
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:SHUFFLE] Error", code, result);
+          else logSpotify("DONE_SHUFFLE");
+        });
+        break;
       case "SPOTIFY_REPEAT":
         this.spotify.repeat(payload, (code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:REPEAT] Error", code, result)
-          else logSpotify("DONE_REPEAT")
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:REPEAT] Error", code, result);
+          else logSpotify("DONE_REPEAT");
+        });
+        break;
       case "SPOTIFY_SEEK":
         this.spotify.seek(payload, (code, error, result) => {
-          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:SEEK] Error", code, result)
-          else logSpotify("DONE_SEEK", payload)
-        })
-        break
+          if ((code !== 204) && (code !== 202)) console.log("[SPOTIFY:SEEK] Error", code, result);
+          else logSpotify("DONE_SEEK", payload);
+        });
+        break;
       case "SEARCH_AND_PLAY":
-        logSpotify("Search and Play", payload)
-        this.searchAndPlay(payload.query, payload.condition)
-        break
+        logSpotify("Search and Play", payload);
+        this.searchAndPlay(payload.query, payload.condition);
+        break;
       case "ASK_DEVICES":
-        this.spotify.updateDeviceList()
-        break
+        this.spotify.updateDeviceList();
+        break;
     }
   },
 
-  initialize: async function (config) {
-    this.config = config
-    if (this.config.debug) logSpotify = (...args) => { console.log("[SPOTIFY]", ...args) }
-    logSpotify("Starting Spotify module...")
+  async initialize (config) {
+    this.config = config;
+    if (this.config.debug) logSpotify = (...args) => { console.log("[SPOTIFY]", ...args); };
+    logSpotify("Starting Spotify module...");
     try {
       this.spotify = new spotify(this.config.visual,
         (noti, params) => {
-          this.sendSocketNotification(noti, params)
-        }, this.config.debug, false, this.config.SCL
-      )
-      this.spotify.start()
+          this.sendSocketNotification(noti, params);
+        },
+        this.config.debug,
+        false,
+        this.config.SCL);
+      this.spotify.start();
     } catch (e) {
-      let error = "SPOTIFY: tokenSpotify.json file not found !"
-      console.log("[SPOTIFY] Error From library: " + e)
-      this.sendSocketNotification("WARNING" , {  message: error })
+      let error = "SPOTIFY: tokenSpotify.json file not found !";
+      console.log(`[SPOTIFY] Error From library: ${  e}`);
+      this.sendSocketNotification("WARNING" , {  message: error });
     }
   },
 
   /** Spotify Search sub-function **/
-  searchAndPlay: function (param, condition) {
+  searchAndPlay (param, condition) {
     if (!param.type) {
-      param.type = "artist,track,album,playlist"
+      param.type = "artist,track,album,playlist";
     } else {
-      param.type = param.type.replace(/\s/g, '')
+      param.type = param.type.replace(/\s/g, "");
     }
     if (!param.q) {
-      param.q = "something cool"
+      param.q = "something cool";
     }
     var pickup = (items, random, retType) => {
-      var ret = {}
+      var ret = {};
       var r = (random)
         ? items[Math.floor(Math.random() * items.length)]
-        : items[0]
-        if (r.uri) {
-          ret[retType] = (retType == "uris") ? [r.uri] : r.uri
-          return ret
-        } else {
-          console.log("[SPOTIFY] Unplayable item: ", r)
-          return false
+        : items[0];
+      if (r.uri) {
+        ret[retType] = (retType === "uris") ? [r.uri] : r.uri;
+        return ret;
+      } else {
+        console.log("[SPOTIFY] Unplayable item: ", r);
+        return false;
       }
-    }
+    };
     this.spotify.search(param, (code, error, result) => {
-      var foundForPlay = null
-      if (code == 200) { //When success
+      var foundForPlay = null;
+      if (code === 200) { //When success
         const map = {
-          "tracks": "uris",
-          "artists": "context_uri",
-          "albums": "context_uri",
-          "playlists": "context_uri"
-        }
+          tracks: "uris",
+          artists: "context_uri",
+          albums: "context_uri",
+          playlists: "context_uri"
+        };
         for (var section in map) {
           if (map.hasOwnProperty(section) && !foundForPlay) {
-            var retType = map[section]
+            var retType = map[section];
             if (result[section] && result[section].items.length > 1) {
-              foundForPlay = pickup(result[section].items, condition.random, retType)
+              foundForPlay = pickup(result[section].items, condition.random, retType);
             }
           }
         }
         if (foundForPlay && condition.autoplay) {
-          logSpotify("Search and Play Result:", foundForPlay)
-          this.socketNotificationReceived("SPOTIFY_PLAY", foundForPlay)
+          logSpotify("Search and Play Result:", foundForPlay);
+          this.socketNotificationReceived("SPOTIFY_PLAY", foundForPlay);
         } else {
-          logSpotify("Search and Play No Result")
-          this.sendSocketNotification("WARNING" , { message: "SpotifyNoResult" })
+          logSpotify("Search and Play No Result");
+          this.sendSocketNotification("WARNING" , { message: "SpotifyNoResult" });
         }
       } else { //when fail
-        console.log("[SPOTIFY] Search and Play failed !")
-        this.sendSocketNotification("WARNING" , { message: "SpotifySearchFailed" })
+        console.log("[SPOTIFY] Search and Play failed !");
+        this.sendSocketNotification("WARNING" , { message: "SpotifySearchFailed" });
       }
-    })
+    });
   },
 
-  DisplayError: function (err, error, details = null) {
-    if (details) console.log("[SPOTIFY][ERROR]" + err, details.message, details)
-    else console.log("[SPOTIFY][ERROR]" + err)
-    return this.sendSocketNotification("NOT_INITIALIZED", { message: error.message, values: error.values })
+  DisplayError (err, error, details = null) {
+    if (details) console.log(`[SPOTIFY][ERROR]${err}`, details.message, details);
+    else console.log(`[SPOTIFY][ERROR]${err}`);
+    return this.sendSocketNotification("NOT_INITIALIZED", { message: error.message, values: error.values });
   }
-})
+});
